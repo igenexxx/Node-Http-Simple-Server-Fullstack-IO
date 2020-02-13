96fbf44e46f02d0313ef6ae4e5420b87f6703813
Zhenya Sikirzhitsky <igenexxx@gmail.com>
2020-02-14 00:08:23 +0300

    Dynamic url

--- index.js
+++ index.js
@@ -1,10 +1,50 @@
 const http = require('http');
+const querystring = require('querystring');
 
 const port = process.env.PORT || 1337;
 
-const server = http.createServer((req, res) => {
+const respondText = (req, res) => {
+    res.setHeader('Content-Type', 'text/plain');
+    res.end('Hi');
+};
+
+const respondJson = (req, res) => {
     res.setHeader('Content-Type', 'application/json');
-    res.end(JSON.stringify({ text: 'hi', numbers: [1, 2, 3]}))
-}).listen(port);
+    res.end(JSON.stringify({ text: 'Hi', number: [1, 2, 3]}));
+};
+
+const respondNotFound = (req, res) => {
+    res.writeHead(404, { 'Content-Type': 'text/plain' });
+    res.end('Not Found');
+};
+
+const respondEcho = (req, res) => {
+    const { input = '' } = querystring.parse(req.url.slice(req.url.indexOf('?') + 1));
+
+    res.setHeader('Content-Type', 'application/json');
+    res.end(JSON.stringify({
+        normal: input,
+        shouty: input.toUpperCase(),
+        characterCount: input.length,
+        backwards: input.split('').reverse().join(''),
+    }))
+};
+
+const server = http.createServer((req, res) => {
+    switch(req.url) {
+        case '/':
+            return respondText(req, res);
+        case '/json':
+            return respondJson(req, res);
+    }
+
+    if (req.url.match(/^\/echo/)) {
+        return respondEcho(req, res);
+    }
+
+
+    return respondNotFound(req, res);
+})
+    .listen(port);
 
 console.log(`Server listening on port ${port}`);
